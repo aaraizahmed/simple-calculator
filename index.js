73e358ad98b796ab45e1c64d767bcbc5eddba3f9
SYED AHMED ALI <166899274+aaraizahmed@users.noreply.github.com>
2024-04-22 10:34:58 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+import inquirer from "inquirer";
+//Asking Question from Users through Inquirer
+const answers = await inquirer.prompt([
+    { message: "Enter first Number", type: "number", name: "firstNumber" },
+    { message: "Enter second Number", type: "number", name: "secondNumber" },
+    {
+        message: "Select one Operator to perform Operations",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtractions", "Multiplication", "Division"],
+    },
+]);
+// Conditional statements If-Else
+if (answers.operator === "Addition") {
+    console.log(answers.firstNumber + answers.secondNumber);
+}
+else if (answers.operator === "Subtractions") {
+    console.log(answers.firstNumber - answers.secondNumber);
+}
+else if (answers.operator === "Multiplication") {
+    console.log(answers.firstNumber * answers.secondNumber);
+}
+else if (answers.operator === "Division") {
+    console.log(answers.firstNumber / answers.secondNumber);
+}
+else {
+    console.log("Invalid Input");
+}
